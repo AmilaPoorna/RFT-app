d6cce8c1fdfb5f7926144b44ea63fd3de1090340
Amila Poorna <arunodamkap@gmail.com>
2025-02-19 17:16:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,18 +15,23 @@ r_X_train = joblib.load("r_X_train.pkl")
 
 st.title('Nylon Dyeing Recipe Status Predictor')
 
-recipe_quantity = st.number_input('Recipe Quantity (kg)', min_value=0.001, step=0.001)
-colour_shade = st.selectbox('Colour Shade', ['Very Light', 'Light', 'Medium', 'Dark', 'Very Dark'])
-first_colour = st.radio('First Colour', ['Yes', 'No'])
-colour_description = st.selectbox('Colour Description', ['Normal', 'Softner', 'Special Colour'])
-lab_dip = st.radio('Lab Dip', ['Yes', 'No'])
-nylon_type = st.selectbox('Nylon Type', ['Stretch Nylon', 'Micro Fiber Streatch Nylon', 'Other'])
-denier = st.selectbox('Denier', [44, 70, 78, 100])
-dyeing_method = st.selectbox('Dyeing Method', ['Bullet', 'Hank', 'Package'])
+def reset_prediction():
+    st.session_state.prediction_class = None
+    st.session_state.show_cost_section = False
+
+recipe_quantity = st.number_input('Recipe Quantity (kg)', min_value=0.001, step=0.001, key="recipe_quantity", on_change=reset_prediction)
+colour_shade = st.selectbox('Colour Shade', ['Very Light', 'Light', 'Medium', 'Dark', 'Very Dark'], key="colour_shade", on_change=reset_prediction)
+first_colour = st.radio('First Colour', ['Yes', 'No'], key="first_colour", on_change=reset_prediction)
+colour_description = st.selectbox('Colour Description', ['Normal', 'Softner', 'Special Colour'], key="colour_description", on_change=reset_prediction)
+lab_dip = st.radio('Lab Dip', ['Yes', 'No'], key="lab_dip", on_change=reset_prediction)
+nylon_type = st.selectbox('Nylon Type', ['Stretch Nylon', 'Micro Fiber Streatch Nylon', 'Other'], key="nylon_type", on_change=reset_prediction)
+denier = st.selectbox('Denier', [44, 70, 78, 100], key="denier", on_change=reset_prediction)
+dyeing_method = st.selectbox('Dyeing Method', ['Bullet', 'Hank', 'Package'], key="dyeing_method", on_change=reset_prediction)
 colour = st.selectbox('Colour', ['Black', 'White', 'Grey', 'Blue', 'Navy Blue', 'Green', 'Pink', 'Red',
-                                 'Beige', 'Orange', 'Brown', 'Purple', 'Cream', 'Yellow', 'Maroon', 'Other'])
+                                 'Beige', 'Orange', 'Brown', 'Purple', 'Cream', 'Yellow', 'Maroon', 'Other'], key="colour", on_change=reset_prediction)
 machine_capacity = st.selectbox('Machine Capacity (Packages)', [1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 16, 24, 28, 30, 36, 42,
-                                                                 25, 48, 54, 56, 75, 90, 104, 108, 132, 216, 264, 432, 558, 981])
+                                                                 25, 48, 54, 56, 75, 90, 104, 108, 132, 216, 264, 432, 558, 981],
+                                key="machine_capacity", on_change=reset_prediction)
 
 if 'prediction_class' not in st.session_state:
     st.session_state.prediction_class = None
