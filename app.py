4e49c209aad28f0b649364a7f5341cea1a303e93
Amila Poorna <arunodamkap@gmail.com>
2025-02-19 09:49:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ if st.button('Predict'):
         rft_data[col] = False
     rft_data = rft_data[c_X_train]
     rft_data['RecipeQty'] = c_scaler.transform(rft_data[['RecipeQty']])
-    rft_data['Denier'] = c_denier_encoder.transform(rft_data['Denier'])
+    #rft_data['Denier'] = c_denier_encoder.transform(rft_data['Denier'])
     rft_data['MachineCapacity(Packages)'] = c_capacity_encoder.transform(rft_data['MachineCapacity(Packages)'])
 
     prediction_class = classification_model.predict(rft_data)
