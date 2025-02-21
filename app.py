68fc64aa498ba550e3ae3e2780f0819d3476889b
Amila Poorna <arunodamkap@gmail.com>
2025-02-21 10:39:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,17 +41,17 @@ def reset_prediction():
     st.session_state.prediction_class = None
     st.session_state.show_cost_section = False
 
-recipe_quantity = st.number_input('Recipe Quantity (kg)', min_value=0.001, step=0.001, format="%.3f", key="recipe_quantity", on_change=reset_prediction)
-colour_shade = st.selectbox('Colour Shade', ['Very Light', 'Light', 'Medium', 'Dark', 'Very Dark'], key="colour_shade", on_change=reset_prediction)
-first_colour = st.radio('First Colour', ['Yes', 'No'], key="first_colour", on_change=reset_prediction)
-colour_description = st.selectbox('Colour Description', ['Normal', 'Softner', 'Special Colour'], key="colour_description", on_change=reset_prediction)
-lab_dip = st.radio('Lab Dip', ['Yes', 'No'], key="lab_dip", on_change=reset_prediction)
-nylon_type = st.selectbox('Nylon Type', ['Stretch Nylon', 'Micro Fiber Streatch Nylon', 'Other'], key="nylon_type", on_change=reset_prediction)
-denier = st.selectbox('Denier', [44, 70, 78, 100], key="denier", on_change=reset_prediction)
-dyeing_method = st.selectbox('Dyeing Method', ['Bullet', 'Hank', 'Package'], key="dyeing_method", on_change=reset_prediction)
-colour = st.selectbox('Colour', ['Black', 'White', 'Grey', 'Blue', 'Navy Blue', 'Green', 'Pink', 'Red',
+recipe_quantity = st.number_input('Enter Recipe Quantity (kg):', min_value=0.001, step=0.001, format="%.3f", key="recipe_quantity", on_change=reset_prediction)
+colour_shade = st.selectbox('Select the Colour Shade:', ['Very Light', 'Light', 'Medium', 'Dark', 'Very Dark'], key="colour_shade", on_change=reset_prediction)
+first_colour = st.radio('Is the Color Being Created for the First Time in the Dye Lab?', ['Yes', 'No'], key="first_colour", on_change=reset_prediction)
+colour_description = st.selectbox('Select the Colour Description:', ['Normal', 'Softner', 'Special Colour'], key="colour_description", on_change=reset_prediction)
+lab_dip = st.radio('Is the Swatch Being Created in the Dye Lab?', ['Yes', 'No'], key="lab_dip", on_change=reset_prediction)
+nylon_type = st.selectbox('Select the Nylon Type:', ['Stretch Nylon', 'Micro Fiber Streatch Nylon', 'Other'], key="nylon_type", on_change=reset_prediction)
+denier = st.selectbox('Select the Denier Count:', [44, 70, 78, 100], key="denier", on_change=reset_prediction)
+dyeing_method = st.selectbox('Select the Dyeing Method:', ['Bullet', 'Hank', 'Package'], key="dyeing_method", on_change=reset_prediction)
+colour = st.selectbox('Select the Colour:', ['Black', 'White', 'Grey', 'Blue', 'Navy Blue', 'Green', 'Pink', 'Red',
                                  'Beige', 'Orange', 'Brown', 'Purple', 'Cream', 'Yellow', 'Maroon', 'Other'], key="colour", on_change=reset_prediction)
-machine_capacity = st.selectbox('Machine Capacity (Packages)', [1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 16, 24, 28, 30, 36, 42,
+machine_capacity = st.selectbox('Select the Machine Capacity (Packages):', [1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 16, 24, 28, 30, 36, 42,
                                                                  25, 48, 54, 56, 75, 90, 104, 108, 132, 216, 264, 432, 558, 981],
                                 key="machine_capacity", on_change=reset_prediction)
 
@@ -111,8 +111,8 @@ if st.session_state.prediction_class is not None:
     st.write(f"Prediction: {prediction_label}")
 
 if st.session_state.show_cost_section:
-    supplier = st.selectbox('Supplier', ['Rudolf', 'Ohyoung', 'Harris & Menuk'], key="supplier")
-    iso_105 = st.radio('ISO 105', ['Yes', 'No'], key="iso_105")
+    supplier = st.selectbox('Select the Supplier:', ['Rudolf', 'Ohyoung', 'Harris & Menuk'], key="supplier")
+    iso_105 = st.radio('Does the dyestuff have high fastness ratings in ISO 105 series tests?', ['Yes', 'No'], key="iso_105")
 
     if st.button('Predict Cost'):
         cost_data = pd.DataFrame({
